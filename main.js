12e9f64ae3a214ff69d99871ef3cc7735fc82c31
Joshua Caron <joshua@joshuacaron.ca>
2015-06-17 14:00:31 -0400

    Split off deeply nested code into helper function. Added argument to function to remove newlines on final pass to minimize the code.

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var katex = require('katex')
 var fs = require('fs')
 var _ = require('lodash')
 
-var parseExpression = function(raw, delimit, delimitEscaped, mathMode) {
+var parseExpression = function(raw, delimit, delimitEscaped, mathMode, finalPass) {
+  finalPass = finalPass || false
   var lines = raw.split('\n')
   var output = ''
   for (var j = 0; j < lines.length; j++) {
@@ -23,36 +24,41 @@ var parseExpression = function(raw, delimit, delimitEscaped, mathMode) {
 
     if (splitLine.length > 1 && splitLine.length % 2 === 1) {
       // If there were matches and the code is well-formed, parse each math section (odd indices)
-      for (var i = 0; i < splitLine.length; ++i) {
-        if (i % 2 === 0) {
-          parsedLine += splitLine[i]
-        } else {
-          try {
-            parsedLine += katex.renderToString(splitLine[i],{displayMode: mathMode})
-          }
-          // Render unformatted text if there is an error
-          catch (err) {
-            parsedLine += '<p style=\"text-align:center;\">' + delimitEscaped + splitLine[i] + delimitEscaped + '</p>'
-          }
-        }
-      }
+      parsedLine = processLine(splitLine)
     } else {
       // If the LaTeX isn't wellformed (need matched $$s and at least 2), don't parse the line.
       parsedLine = lines[j]
     }
     // Sum up the resulting lines and add newlines back in
-    output += j < lines.length - 1 ? parsedLine + '\n' : parsedLine
+    output += j < lines.length - 1 && !finalPass ? parsedLine + '\n' : parsedLine
   }
   return output
+
+  function processLine(rawLine){
+    var parsedLine = ''
+    for (var i = 0; i < rawLine.length; ++i) {
+      if (i % 2 === 0) {
+        parsedLine += rawLine[i]
+      } else {
+        try {
+          parsedLine += katex.renderToString(rawLine[i],{displayMode: mathMode})
+        }
+        // Render unformatted text if there is an error
+        catch (err) {
+          var original = delimitEscaped + rawLine[i] + delimitEscaped
+          parsedLine = mathMode ? '<p style=\"text-align:center;\">' + original + "<p>" : original
+        }
+      }
+    }
+
+    return parsedLine
+  }
 }
 
 var renderLaTeX = function(unparsed) {
   // Need to parse for $$ first so it doesn't cause problems when check for $
   var parsed = parseExpression(unparsed, '$$', '\$\$', true)
-  parsed = parseExpression(parsed, '$', '\$', false)
-
-  // Remove final newlines, to minimize the code
-  parsed = parsed.replace(/\n/g, '')
+  parsed = parseExpression(parsed, '$', '\$', false, true)
 
   // Render escaped dollar signs back to $
   parsed = parsed.replace(/\\\$/g, '$')
